974a445f94850f54d00a1766be2330c085b7f1b8
Common_fig <gasfel95@likelion.org>
2019-08-07 16:56:37 +0900

    Day5 Add_db.js(test)_videoBlock.pug_socialLogin.pug Modify_routers_views_controller

--- youtube/controllers/videoController.js
+++ youtube/controllers/videoController.js
@@ -1,6 +1,13 @@
-export const home = (req, res) => res.render("home", { pageTitle: "home" });
-export const search = (req, res) =>
-    res.render("search", { pageTitle: "search" });
+import { videos } from "../db";
+
+export const home = (req, res) =>
+    res.render("home", { pageTitle: "home", videos });
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: searchingBy, searchingBy });
+};
 export const upload = (req, res) =>
     res.render("upload", { pageTitle: "upload" });
 export const videoDetail = (req, res) =>
